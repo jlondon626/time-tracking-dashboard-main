ed8a5bf19ac48903e28f5066f2b5c6dc7c389179
Jack <jlondon626@hotmail.co.uk>
2026-06-27 17:05:26 +0100

    add js

---
+++ assets/js/index.js
@@ -0,0 +1,67 @@
+let selectedTimeFrame = "weekly"
+let dashboardData = []
+
+fetch("data.json")
+    .then((response) => {
+        if(!response.ok) {
+            throw new Error("Could not fetch data")
+        }
+        return response.json()
+    })
+    .then((data) => {
+        dashboardData = data;
+        UpdateDOM(selectedTimeFrame)
+    });
+    
+const removeArticles = () => {
+    const timeTrackingContainers = document.querySelectorAll(".time-tracking");
+    timeTrackingContainers.forEach((container) => {
+        container.remove();
+    });
+}
+
+const appendItem = (item, timeframePeriod) => {
+    const timeTrackingContainer = document.createElement("article");
+    const itemSlug = item.title.toLowerCase().replace(/\s+/g, "-");
+
+    timeTrackingContainer.classList.add("time-tracking", `time-tracking-${itemSlug}`);
+
+    
+    timeTrackingContainer.innerHTML = `
+    <img src="assets/images/icon-${itemSlug}.svg" alt="">
+    <div class="time-tracking__content">
+    <div class="time-tracking__header">
+    <h2>${item.title}</h2>
+    <button aria-label="More options"><img src="assets/images/icon-ellipsis.svg" alt=""></button>
+    </div>
+    <div class="time-tracking__hours">
+    <h1 class="current">${item.timeframes[timeframePeriod].current}hrs</h1>
+    <p class="previous">Previous - ${item.timeframes[timeframePeriod].previous}hrs</p>
+    </div>
+    </div>
+    </article>
+    `;
+    
+    document.querySelector(".dashboard").appendChild(timeTrackingContainer);
+}
+
+function UpdateDOM(timeFrame) {
+        removeArticles();
+        for (const item of dashboardData) {
+            appendItem(item, timeFrame)
+        }
+    }
+
+const timeframeButtons = document.querySelectorAll("[data-timeframe]");
+
+timeframeButtons.forEach((timeFrameButton) => {
+    timeFrameButton.addEventListener('click', () => {
+        for (const button of timeframeButtons) {
+            button.classList.remove('active')
+        }
+
+        timeFrameButton.classList.add('active');
+        selectedTimeFrame = timeFrameButton.dataset.timeframe;
+        UpdateDOM(selectedTimeFrame);
+    })
+});
